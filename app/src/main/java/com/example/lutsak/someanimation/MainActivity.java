d12dbc46e10fbc9d5ee11340cbbbec47713559ed
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-04-10 18:49:05 +0300

    Waiting for revision

--- app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
+++ app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
@@ -13,6 +13,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int DEFAULT_DURATION = 3000;
+    public static final int ALPHA_FROM = 0;
+    public static final int ALPHA_TO = 1;
+    public static final int ROTATION_TO = 360;
+    public static final int ROTATION_BACK = 0;
+    public static final float SCALE_XY_TO = 0.5f;
+    public static final float SCALE_XY_BACK_TO = 1f;
+    public static final float TRANSLATION_XY_TO = -100;
+    public static final float TRANSLATION_XY_BACK_TO = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final TextView title = findViewById(R.id.animator_text);
 
         switch (groupClass.getCheckedRadioButtonId()) {
-
+            // Animate our textView using ANIMATION and load animation from xml file
             case R.id.radioButtonAnimation:
 
                 switch (v.getId()) {
@@ -49,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         break;
                 }
                 title.startAnimation(animation);
-
+            // Animate textView using Animator and load animation from xml using AnimatorInflater
             case R.id.radioButtonAnimator:
 
                 switch (v.getId()) {
@@ -75,7 +85,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         animator.start();
                         break;
                 }
-
+            /* Animate our textView using ViewPropertyAnimator
+              (using Runnable we add one more animation that reverse our first)*/
             case R.id.radioButtonViewPropertyAnimator:
 
                 switch (v.getId()){
@@ -83,39 +94,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     case R.id.animation_alpha:
                         Runnable reversAlpha = new Runnable() {
                             public void run() {
-                                title.animate().alpha(1).setDuration(3000);
+                                title.animate()
+                                        .alpha(ALPHA_TO)
+                                        .setDuration(DEFAULT_DURATION);
                             }
                         };
-                        title.animate().alpha(0).setDuration(3000).withEndAction(reversAlpha);
+                        title.animate()
+                                .alpha(ALPHA_FROM)
+                                .setDuration(DEFAULT_DURATION)
+                                .withEndAction(reversAlpha);
                         break;
                     case R.id.animation_rotate:
-                        title.animate().rotation(360).setDuration(3000);
+                        Runnable reversRotation = new Runnable() {
+                            @Override
+                            public void run() {
+                                title.animate()
+                                        .rotation(ROTATION_BACK)
+                                        .setDuration(DEFAULT_DURATION);
+                            }
+                        };
+                        title.animate()
+                                .rotation(ROTATION_TO)
+                                .setDuration(DEFAULT_DURATION)
+                                .withEndAction(reversRotation);
                         break;
                     case R.id.animation_scale:
                         Runnable reversScale = new Runnable() {
                             @Override
                             public void run() {
-                                title.animate().scaleX(1).scaleY(1).setDuration(3000);
+                                title.animate()
+                                        .scaleX(SCALE_XY_BACK_TO)
+                                        .scaleY(SCALE_XY_BACK_TO)
+                                        .setDuration(DEFAULT_DURATION);
                             }
                         };
                         title.animate()
-                                .scaleX(0.5f)
-                                .scaleY(0.5f)
-                                .setDuration(3000)
+                                .scaleX(SCALE_XY_TO)
+                                .scaleY(SCALE_XY_TO)
+                                .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reversScale);
                         break;
-
                     case R.id.animation_translate:
                         Runnable reverseTranslate = new Runnable() {
                             @Override
                             public void run() {
-                                title.animate().translationX(0).translationY(0).setDuration(3000);
+                                title.animate()
+                                        .translationX(TRANSLATION_XY_BACK_TO)
+                                        .translationY(TRANSLATION_XY_BACK_TO)
+                                        .setDuration(DEFAULT_DURATION);
                             }
                         };
                         title.animate()
-                                .translationX(-100)
-                                .translationY(-100)
-                                .setDuration(3000)
+                                .translationX(TRANSLATION_XY_TO)
+                                .translationY(TRANSLATION_XY_TO)
+                                .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reverseTranslate);
                         break;
                 }
