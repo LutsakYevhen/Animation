be82eff50035c97d87c22677d9432d4d6f3a9524
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-04-25 11:05:21 +0300

    Updated gradle and added Logs

--- app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
+++ app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,6 +13,8 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public static final int DEFAULT_DURATION = 3000;
     public static final int ALPHA_FROM = 0;
@@ -28,11 +31,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, ">> onCreate");
+
         getElements();
+
+        Log.d(TAG, "<< onCreate");
     }
 
     @Override
     public void onClick(View v) {
+
+        Log.d(TAG, ">> onClick");
 
         Animation animation = null;
         Animator animator;
@@ -43,24 +52,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Animate our textView using ANIMATION and load animation from xml file
             case R.id.radioButtonAnimation:
 
+                Log.d(TAG, "Radio button Animation selected:");
+
                 switch (v.getId()) {
 
                     case R.id.animation_alpha:
                         animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+                        Log.d(TAG, "Animation Alpha done");
                         break;
                     case R.id.animation_rotate:
                         animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+                        Log.d(TAG, "Animation Rotate done");
                         break;
                     case R.id.animation_scale:
                         animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+                        Log.d(TAG, "Animation Scale done");
                         break;
                     case R.id.animation_translate:
                         animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+                        Log.d(TAG, "Animation Translate done");
                         break;
                 }
                 title.startAnimation(animation);
+                break;
             // Animate textView using Animator and load animation from xml using AnimatorInflater
             case R.id.radioButtonAnimator:
+
+                Log.d(TAG, "Radio button Animator selected:");
 
                 switch (v.getId()) {
 
@@ -68,26 +86,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         animator = AnimatorInflater.loadAnimator(this, R.animator.alpha);
                         animator.setTarget(title);
                         animator.start();
+                        Log.d(TAG, "Animator alpha done");
                         break;
                     case R.id.animation_rotate:
                         animator = AnimatorInflater.loadAnimator(this, R.animator.rotate);
                         animator.setTarget(title);
                         animator.start();
+                        Log.d(TAG, "Animator rotate done");
                         break;
                     case R.id.animation_scale:
                         animator = AnimatorInflater.loadAnimator(this, R.animator.scale);
                         animator.setTarget(title);
                         animator.start();
+                        Log.d(TAG, "Animator scale done");
                         break;
                     case R.id.animation_translate:
                         animator = AnimatorInflater.loadAnimator(this, R.animator.translate);
                         animator.setTarget(title);
                         animator.start();
+                        Log.d(TAG, "Animator translate done");
                         break;
                 }
+                break;
             /* Animate our textView using ViewPropertyAnimator
               (using Runnable we add one more animation that reverse our first)*/
             case R.id.radioButtonViewPropertyAnimator:
+
+                Log.d(TAG, "Radio button ViewPropertyAnimator selected:");
 
                 switch (v.getId()){
 
@@ -103,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 .alpha(ALPHA_FROM)
                                 .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reversAlpha);
+                        Log.d(TAG, "ViewPropertyAnimator alpha done");
                         break;
                     case R.id.animation_rotate:
                         Runnable reversRotation = new Runnable() {
@@ -117,6 +143,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 .rotation(ROTATION_TO)
                                 .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reversRotation);
+                        Log.d(TAG, "ViewPropertyAnimator rotate done");
                         break;
                     case R.id.animation_scale:
                         Runnable reversScale = new Runnable() {
@@ -133,6 +160,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 .scaleY(SCALE_XY_TO)
                                 .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reversScale);
+                        Log.d(TAG, "ViewPropertyAnimator scale done");
                         break;
                     case R.id.animation_translate:
                         Runnable reverseTranslate = new Runnable() {
@@ -149,9 +177,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 .translationY(TRANSLATION_XY_TO)
                                 .setDuration(DEFAULT_DURATION)
                                 .withEndAction(reverseTranslate);
+                        Log.d(TAG, "ViewPropertyAnimator translate done");
                         break;
                 }
+                break;
         }
+        Log.d(TAG, "<< onClick");
     }
 
     private void getElements() {
