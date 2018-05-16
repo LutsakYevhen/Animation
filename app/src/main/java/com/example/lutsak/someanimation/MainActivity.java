0473fa6ee7dda45b94c2cf0f6e6686259dddb5bc
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-05-16 12:27:58 +0300

    made some adjustment

--- app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
+++ app/src/main/java/com/example/lutsak/someanimation/MainActivity.java
@@ -26,6 +26,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static final float TRANSLATION_XY_TO = -100;
     public static final float TRANSLATION_XY_BACK_TO = 0;
 
+    TextView title;
+    Button alphaButton;
+    Button rotateButton;
+    Button scaleButton;
+    Button translateButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +45,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
         Log.d(TAG, ">> onClick");
 
-        Animation animation = null;
-        Animator animator;
         RadioGroup groupClass = findViewById(R.id.radio_group_class);
-        final TextView title = findViewById(R.id.animator_text);
 
         switch (groupClass.getCheckedRadioButtonId()) {
             // Animate our textView using ANIMATION and load animation from xml file
@@ -54,59 +57,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Log.d(TAG, "Radio button Animation selected:");
 
-                switch (v.getId()) {
-
-                    case R.id.animation_alpha:
-                        animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
-                        Log.d(TAG, "Animation Alpha done");
-                        break;
-                    case R.id.animation_rotate:
-                        animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
-                        Log.d(TAG, "Animation Rotate done");
-                        break;
-                    case R.id.animation_scale:
-                        animation = AnimationUtils.loadAnimation(this, R.anim.scale);
-                        Log.d(TAG, "Animation Scale done");
-                        break;
-                    case R.id.animation_translate:
-                        animation = AnimationUtils.loadAnimation(this, R.anim.translate);
-                        Log.d(TAG, "Animation Translate done");
-                        break;
-                }
-                title.startAnimation(animation);
+                getAnimation(view);
                 break;
+
             // Animate textView using Animator and load animation from xml using AnimatorInflater
             case R.id.radioButtonAnimator:
 
                 Log.d(TAG, "Radio button Animator selected:");
 
-                switch (v.getId()) {
-
-                    case R.id.animation_alpha:
-                        animator = AnimatorInflater.loadAnimator(this, R.animator.alpha);
-                        animator.setTarget(title);
-                        animator.start();
-                        Log.d(TAG, "Animator alpha done");
-                        break;
-                    case R.id.animation_rotate:
-                        animator = AnimatorInflater.loadAnimator(this, R.animator.rotate);
-                        animator.setTarget(title);
-                        animator.start();
-                        Log.d(TAG, "Animator rotate done");
-                        break;
-                    case R.id.animation_scale:
-                        animator = AnimatorInflater.loadAnimator(this, R.animator.scale);
-                        animator.setTarget(title);
-                        animator.start();
-                        Log.d(TAG, "Animator scale done");
-                        break;
-                    case R.id.animation_translate:
-                        animator = AnimatorInflater.loadAnimator(this, R.animator.translate);
-                        animator.setTarget(title);
-                        animator.start();
-                        Log.d(TAG, "Animator translate done");
-                        break;
-                }
+                getAnimator(view);
                 break;
             /* Animate our textView using ViewPropertyAnimator
               (using Runnable we add one more animation that reverse our first)*/
@@ -114,87 +73,151 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 Log.d(TAG, "Radio button ViewPropertyAnimator selected:");
 
-                switch (v.getId()){
-
-                    case R.id.animation_alpha:
-                        Runnable reversAlpha = new Runnable() {
-                            public void run() {
-                                title.animate()
-                                        .alpha(ALPHA_TO)
-                                        .setDuration(DEFAULT_DURATION);
-                            }
-                        };
-                        title.animate()
-                                .alpha(ALPHA_FROM)
-                                .setDuration(DEFAULT_DURATION)
-                                .withEndAction(reversAlpha);
-                        Log.d(TAG, "ViewPropertyAnimator alpha done");
-                        break;
-                    case R.id.animation_rotate:
-                        Runnable reversRotation = new Runnable() {
-                            @Override
-                            public void run() {
-                                title.animate()
-                                        .rotation(ROTATION_BACK)
-                                        .setDuration(DEFAULT_DURATION);
-                            }
-                        };
-                        title.animate()
-                                .rotation(ROTATION_TO)
-                                .setDuration(DEFAULT_DURATION)
-                                .withEndAction(reversRotation);
-                        Log.d(TAG, "ViewPropertyAnimator rotate done");
-                        break;
-                    case R.id.animation_scale:
-                        Runnable reversScale = new Runnable() {
-                            @Override
-                            public void run() {
-                                title.animate()
-                                        .scaleX(SCALE_XY_BACK_TO)
-                                        .scaleY(SCALE_XY_BACK_TO)
-                                        .setDuration(DEFAULT_DURATION);
-                            }
-                        };
-                        title.animate()
-                                .scaleX(SCALE_XY_TO)
-                                .scaleY(SCALE_XY_TO)
-                                .setDuration(DEFAULT_DURATION)
-                                .withEndAction(reversScale);
-                        Log.d(TAG, "ViewPropertyAnimator scale done");
-                        break;
-                    case R.id.animation_translate:
-                        Runnable reverseTranslate = new Runnable() {
-                            @Override
-                            public void run() {
-                                title.animate()
-                                        .translationX(TRANSLATION_XY_BACK_TO)
-                                        .translationY(TRANSLATION_XY_BACK_TO)
-                                        .setDuration(DEFAULT_DURATION);
-                            }
-                        };
-                        title.animate()
-                                .translationX(TRANSLATION_XY_TO)
-                                .translationY(TRANSLATION_XY_TO)
-                                .setDuration(DEFAULT_DURATION)
-                                .withEndAction(reverseTranslate);
-                        Log.d(TAG, "ViewPropertyAnimator translate done");
-                        break;
-                }
+                getViewPropertyAnimator(view);
                 break;
         }
         Log.d(TAG, "<< onClick");
     }
 
+    private void getAnimation(View view){
+        Animation animation = null;
+
+        switch (view.getId()) {
+
+            case R.id.animation_alpha:
+                animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+                Log.d(TAG, "Animation Alpha done");
+                break;
+            case R.id.animation_rotate:
+                animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+                Log.d(TAG, "Animation Rotate done");
+                break;
+            case R.id.animation_scale:
+                animation = AnimationUtils.loadAnimation(this, R.anim.scale);
+                Log.d(TAG, "Animation Scale done");
+                break;
+            case R.id.animation_translate:
+                animation = AnimationUtils.loadAnimation(this, R.anim.translate);
+                Log.d(TAG, "Animation Translate done");
+                break;
+        }
+        title.startAnimation(animation);
+    }
+
+    private void getAnimator(View view){
+        Animator animator;
+
+        switch (view.getId()) {
+
+            case R.id.animation_alpha:
+                animator = AnimatorInflater.loadAnimator(this, R.animator.alpha);
+                animator.setTarget(title);
+                animator.start();
+                Log.d(TAG, "Animator alpha done");
+                break;
+            case R.id.animation_rotate:
+                animator = AnimatorInflater.loadAnimator(this, R.animator.rotate);
+                animator.setTarget(title);
+                animator.start();
+                Log.d(TAG, "Animator rotate done");
+                break;
+            case R.id.animation_scale:
+                animator = AnimatorInflater.loadAnimator(this, R.animator.scale);
+                animator.setTarget(title);
+                animator.start();
+                Log.d(TAG, "Animator scale done");
+                break;
+            case R.id.animation_translate:
+                animator = AnimatorInflater.loadAnimator(this, R.animator.translate);
+                animator.setTarget(title);
+                animator.start();
+                Log.d(TAG, "Animator translate done");
+                break;
+        }
+    }
+
+    private void getViewPropertyAnimator(View view){
+        switch (view.getId()){
+
+            case R.id.animation_alpha:
+                Runnable reversAlpha = new Runnable() {
+                    public void run() {
+                        title.animate()
+                                .alpha(ALPHA_TO)
+                                .setDuration(DEFAULT_DURATION);
+                    }
+                };
+                title.animate()
+                        .alpha(ALPHA_FROM)
+                        .setDuration(DEFAULT_DURATION)
+                        .withEndAction(reversAlpha);
+                Log.d(TAG, "ViewPropertyAnimator alpha done");
+                break;
+
+            case R.id.animation_rotate:
+                Runnable reversRotation = new Runnable() {
+                    @Override
+                    public void run() {
+                        title.animate()
+                                .rotation(ROTATION_BACK)
+                                .setDuration(DEFAULT_DURATION);
+                    }
+                };
+                title.animate()
+                        .rotation(ROTATION_TO)
+                        .setDuration(DEFAULT_DURATION)
+                        .withEndAction(reversRotation);
+                Log.d(TAG, "ViewPropertyAnimator rotate done");
+                break;
+
+            case R.id.animation_scale:
+                Runnable reversScale = new Runnable() {
+                    @Override
+                    public void run() {
+                        title.animate()
+                                .scaleX(SCALE_XY_BACK_TO)
+                                .scaleY(SCALE_XY_BACK_TO)
+                                .setDuration(DEFAULT_DURATION);
+                    }
+                };
+                title.animate()
+                        .scaleX(SCALE_XY_TO)
+                        .scaleY(SCALE_XY_TO)
+                        .setDuration(DEFAULT_DURATION)
+                        .withEndAction(reversScale);
+                Log.d(TAG, "ViewPropertyAnimator scale done");
+                break;
+
+            case R.id.animation_translate:
+                Runnable reverseTranslate = new Runnable() {
+                    @Override
+                    public void run() {
+                        title.animate()
+                                .translationX(TRANSLATION_XY_BACK_TO)
+                                .translationY(TRANSLATION_XY_BACK_TO)
+                                .setDuration(DEFAULT_DURATION);
+                    }
+                };
+                title.animate()
+                        .translationX(TRANSLATION_XY_TO)
+                        .translationY(TRANSLATION_XY_TO)
+                        .setDuration(DEFAULT_DURATION)
+                        .withEndAction(reverseTranslate);
+                Log.d(TAG, "ViewPropertyAnimator translate done");
+                break;
+        }
+    }
+
     private void getElements() {
-        Button alphaButton = findViewById(R.id.animation_alpha);
-        Button rotateButton = findViewById(R.id.animation_rotate);
-        Button scaleButton = findViewById(R.id.animation_scale);
-        Button translateButton = findViewById(R.id.animation_translate);
+        title = findViewById(R.id.animator_text);
+        alphaButton = findViewById(R.id.animation_alpha);
+        rotateButton = findViewById(R.id.animation_rotate);
+        scaleButton = findViewById(R.id.animation_scale);
+        translateButton = findViewById(R.id.animation_translate);
 
         alphaButton.setOnClickListener(this);
         rotateButton.setOnClickListener(this);
         scaleButton.setOnClickListener(this);
         translateButton.setOnClickListener(this);
     }
-
 }
